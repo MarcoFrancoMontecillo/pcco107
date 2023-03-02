bdfdeda9148fd4488a78c562771e1d261b95b770
Marco Franco Montecillo <montecillo2030311@mls.ceu.edu.ph>
2023-03-02 23:04:58 +0800

    updates

--- activity/index.js
+++ activity/index.js
@@ -5,19 +5,17 @@ function personInformation(){
   if (fullName != null && age != null && location != null) {
     alert ('Thank You For The Informations!')
     console.log(`Hello ${fullName}, with the age of ${age} and located at ${location}`)
+
+      function favBands(firstBand, secondBand, thirdBand, forthBand, fifthBand){
+        return `1st = ${firstBand} 2nd = ${secondBand} 3rd = ${thirdBand} 4th = ${forthBand} 5th = ${fifthBand}`
+      }
+      let favBandsBe = favBands('Ben&Ben', 'Silent Sactuary', 'Queens', 'Eraserheads', 'parokya ni edgar')
+      console.log(favBandsBe);
+
+      function favMovies(firstMovie, secondMovie, thirdMovie, forthMovie, fifthMovie){
+        return `1st = ${firstMovie} 2nd = ${secondMovie} 3rd = ${thirdMovie} 4th = ${forthMovie} 5th = ${fifthMovie}`
+      }
+      let favMoviesBe = favMovies('BLACK ADAM - 39%', 'FANTASTIC BEASTS: THE SECRETS OF DUMBLEDORE - 46%', 'ALL QUIET ON THE WESTERN FRONT - 90%', 'UNCHARTED - 41%', 'SPIDERHEAD - 39%')
+      console.log(favMoviesBe);
   }
 }
-
-function favBands(firstBand, secondBand, thirdBand, forthBand, fifthBand){
-  return `1st = ${firstBand} 2nd = ${secondBand} 3rd = ${thirdBand} 4th = ${forthBand} 5th = ${fifthBand}`
-}
-
-let favBandsBe = favBands('Ben&Ben', 'Silent Sactuary', 'Queens', 'Eraserheads', 'parokya ni edgar')
-console.log(favBandsBe);
-
-function favMovies(firstMovie, secondMovie, thirdMovie, forthMovie, fifthMovie){
-  return `1st = ${firstMovie} 2nd = ${secondMovie} 3rd = ${thirdMovie} 4th = ${forthMovie} 5th = ${fifthMovie}`
-}
-
-let favMoviesBe = favMovies('BLACK ADAM - 39%', 'FANTASTIC BEASTS: THE SECRETS OF DUMBLEDORE - 46%', 'ALL QUIET ON THE WESTERN FRONT - 90%', 'UNCHARTED - 41%', 'SPIDERHEAD - 39%')
-console.log(favMoviesBe);
